796f043a7035c8db9e093a6223709d5b65463b7b
Victor Isidoro <victorisidoro@icloud.com>
2021-08-06 11:52:18 +0100

    User profile created

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import BookList from "./components/BookList";
 import BookDetails from "./components/BookDetails"
 import Login from './components/Login';
+import User from './components/User'
 
 
 
@@ -17,14 +18,13 @@ function App() {
   return (
     <Router>
     <div >
-      <Navbar / >
-      Welcome Anarchists!
-      
+      <Navbar / >      
       {/* Routes */}
       <Switch>
         <Route path='/' exact component={FirstSlider} />
         <Route path='/books/' exact component={BookList} />
         <Route path='/books/:id' exact component={BookDetails} />
+        <Route exact path="/user/" component={User} />
       </Switch>
     </div>
     </Router>
--- src/components/User.js
+++ src/components/User.js
@@ -1,1 +1,39 @@
-const 
+import { useState } from 'react'
+import BookList from './BookList'
+
+const User = () => {
+    const [userName, setUserName] = useState('Leonardo')
+    const [avatar, setAvatar] = useState('/images/avatar4.png')
+
+    return (
+        <div className="pt-10 px-5 md:px-10 lg:px-20 bg-gray-100 h-screen">
+        <div id="profile" className="w-full rounded-lg shadow-2xl bg-white opacity-75">
+		<div className="p-4 md:p-12 text-center">
+			<div className="block rounded-full bg-pink-200 shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style={{backgroundImage: `url(${avatar})` }}>
+            </div>
+			<h1 className="text-3xl font-bold pt-8">{userName}</h1>
+			<div className="mx-auto w-4/5 pt-3 border-b-2 border-indigo-500 opacity-25"></div>
+        </div>
+        <div className="p-2">
+			<p className="p-4 text-base font-bold flex items-center justify-start"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 fill-current text-indigo-700 pr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+</svg> My Favourite Books:</p>
+<div>
+    <BookList isFavourite={true}/>
+</div>
+			<p className="p-4 text-base font-bold flex items-center justify-start"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 fill-current text-indigo-700 pr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+</svg> Books that I have read</p>
+            
+			<div className="pt-8 pb-8 text-center">
+				<button className="bg-indigo-700 hover:bg-indigo-900 text-white font-bold py-2 px-4 rounded-full">
+				  Edit
+				</button> 
+			</div>
+		</div>
+        </div>
+    </div>
+    )
+}
+
+export default User
